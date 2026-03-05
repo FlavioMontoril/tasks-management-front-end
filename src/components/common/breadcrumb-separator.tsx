a8f2aba2eaab187712e3022b9aff45053666dc65
FlavioMontoril <flavio.montorilll@gmail.com>
2026-03-05 15:36:15 -0300

    feat: implemneted breadcrumb

---
+++ src/components/common/breadcrumb-separator.tsx
@@ -0,0 +1,46 @@
+
+import { DotIcon } from "lucide-react"
+import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbSeparator } from "../ui/breadcrumb"
+import { Link } from "react-router-dom"
+import { useLocation } from "react-router-dom";
+
+export function BreadcrumbSeparatorDemo() {
+
+    const { pathname } = useLocation();
+
+    const isActive = (path: string) => pathname === path;
+
+    return (
+        <Breadcrumb>
+            <BreadcrumbList>
+                <BreadcrumbItem>
+                    <BreadcrumbLink asChild>
+                        <Link to="/" className={isActive("/") ? "text-white font-semibold" : "text-muted-foreground"}>
+                            Home
+                        </Link>
+                    </BreadcrumbLink>
+                </BreadcrumbItem>
+                <BreadcrumbSeparator>
+                    <DotIcon />
+                </BreadcrumbSeparator>
+                <BreadcrumbItem>
+                    <BreadcrumbLink asChild>
+                        <Link to="/dashboard" className={isActive("/dashboard") ? "text-white font-semibold" : "text-muted-foreground"}>
+                            Dashboard
+                        </Link>
+                    </BreadcrumbLink>
+                </BreadcrumbItem>
+                <BreadcrumbSeparator>
+                    <DotIcon />
+                </BreadcrumbSeparator>
+                <BreadcrumbItem>
+                    <BreadcrumbLink asChild>
+                        <Link to="/calendar" className={isActive("/calendar") ? "text-white font-semibold" : "text-muted-foreground"}>
+                            Calendar
+                        </Link>
+                    </BreadcrumbLink>
+                </BreadcrumbItem>
+            </BreadcrumbList>
+        </Breadcrumb>
+    )
+}
--- src/layout/app-layout.tsx
+++ src/layout/app-layout.tsx
@@ -9,6 +9,7 @@ import { SidebarInset, SidebarTrigger } from "../components/ui/sidebar";
 import { AppSidebar } from "./AppSidebar";
 import { useSidebar } from "../components/ui/sidebar";
 import { Separator } from "../components/ui/separator";
+import { BreadcrumbSeparatorDemo } from "../components/common/breadcrumb-separator";
 
 export default function AppLayout() {
   const { open, setOpen } = useSidebar();
@@ -53,36 +54,37 @@ export default function AppLayout() {
       <SidebarInset className="min-h-screen w-full flex flex-col bg-gray-900 transition-all duration-900 ease-in-out">
         <div
           className="flex items-center h-10 px-2 md:px-0"
-          //   className="flex items-center h-10 "
+        //   className="flex items-center h-10 "
         >
           <SidebarTrigger className="md:hidden mr-2 text-muted" />
           <Separator
             orientation="vertical"
             className="bg-muted-foreground !h-7"
           />
-          <div>
-            <button
-              title={open ? "Ocultar Menu" : "Exibir Menu"}
-              onClick={() => setOpen(!open)}
-              className="hidden md:flex text-muted-foreground items-center ml-3 cursor-pointer hover:text-white"
-              //   className="text-muted-foreground flex items-center ml-3 cursor-pointer hover:text-white"
-            >
-              <ArrowBigRightDash
-                size={20}
-                className={`
+          <button
+            title={open ? "Ocultar Menu" : "Exibir Menu"}
+            onClick={() => setOpen(!open)}
+            className="hidden md:flex text-muted-foreground items-center ml-3 cursor-pointer hover:text-white"
+          //   className="text-muted-foreground flex items-center ml-3 cursor-pointer hover:text-white"
+          >
+            <ArrowBigRightDash
+              size={20}
+              className={`
                     transform-gpu
                     transition-transform
                     duration-500
                     ease-in-out
                     ${open ? "rotate-180" : "rotate-0"}    `}
-              />
-            </button>
+            />
+          </button>
+          <div className="flex-1 flex justify-center">
+            <BreadcrumbSeparatorDemo />
           </div>
         </div>
 
         <main
           className="flex-1 px-3 py-6  md:px-6 md:py-10 bg-slate-300 rounded-tl-2xl"
-          // className="flex-1 px-3 py-10 bg-slate-300 rounded-tl-2xl"
+        // className="flex-1 px-3 py-10 bg-slate-300 rounded-tl-2xl"
         >
           <Outlet />
         </main>
