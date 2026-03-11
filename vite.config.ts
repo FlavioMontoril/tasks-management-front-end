dd9efbd36b23ea6746f6c171aa0834f8e7172ee4
FlavioMontoril <flavio.montorilll@gmail.com>
2026-03-11 14:59:42 -0300

    feat: containerized application

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,30 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import tailwindcss from '@tailwindcss/vite'
-import path from 'path'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
+  plugins: [react(), tailwindcss()],
+  //Evita problemas de acesso no container.
+  server: {
+    host: true,
+    port: Number(process.env.VITE_PORT) || 5173,
+  },
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
-    }
+      "@": path.resolve(__dirname, "./src"),
+    },
   },
-    build: {
+  build: {
     rollupOptions: {
       output: {
         manualChunks: {
           // Cria um chunk separado só para libs externas grandes
-          vendor: ['react', 'react-dom', 'lucide-react']
-        }
-      }
+          vendor: ["react", "react-dom", "lucide-react"],
+        },
+      },
     },
-    chunkSizeWarningLimit: 600 // aumenta limite para evitar warning de bundle grande
-  }
-})
+    chunkSizeWarningLimit: 600, // aumenta limite para evitar warning de bundle grande
+  },
+});
