d46637709139ce7155cd4dd6462a8216725d7a7b
FlavioMontoril <flavio.montorilll@gmail.com>
2026-03-11 20:36:42 -0300

    refactor filter date

--- src/hooks/useFilteredPagination.tsx
+++ src/hooks/useFilteredPagination.tsx
@@ -1,89 +1,83 @@
-import { endOfDay, startOfDay } from "date-fns"
-import { useMemo, useState, type ChangeEvent } from "react"
+import { endOfDay, startOfDay } from "date-fns";
+import { useMemo, useState, type ChangeEvent } from "react";
 import type { DateRange } from "react-day-picker";
 import { useTaskStore } from "../store/use-task-store";
 
 export function useFilteredPagination() {
+  const { tasks } = useTaskStore();
 
-    const { tasks } = useTaskStore()
+  const [page, setPage] = useState(1);
+  const [search, setSearch] = useState("");
+  const [dateRange, setDateRange] = useState<DateRange | undefined>();
 
-    const [page, setPage] = useState(1)
-    const [search, setSearch] = useState("");
-    const [dateRange, setDateRange] = useState<DateRange | undefined>();
+  const handleSearch = (e: ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+    setPage(1);
+  };
 
-    const handleSearch = (e: ChangeEvent<HTMLInputElement>) => {
-        setSearch(e.target.value)
-        setPage(1)
-    };
+  const handleDateSearch = (range: DateRange | undefined) => {
+    setDateRange(range);
+  };
 
-    const handleDateSearch = (range: DateRange | undefined) => {
-        setDateRange(range)
-    };
+  const filteredData = useMemo(() => {
+    return tasks.filter((task) => {
+      let matchText = true;
+      if (search && search.length >= 3) {
+        matchText =
+          task.codigo
+            .toLocaleLowerCase()
+            .includes(search.toLocaleLowerCase()) ||
+          task.nome.toLocaleLowerCase().includes(search.toLocaleLowerCase());
+      }
 
-    const filteredData = useMemo(() => {
+      let matchDate = true;
+      if (dateRange?.from) {
+        const taskDate = new Date(task.dataCriacao)
 
-        return tasks.filter(task => {
+        const from = startOfDay(dateRange.from);
+        const to = dateRange.to
+          ? endOfDay(dateRange.to)
+          : endOfDay(dateRange.from);
 
-            let matchText = true;
-            if (search && search.length >= 3) {
-                matchText =
-                    task.codigo.toLocaleLowerCase().includes(search.toLocaleLowerCase()) ||
-                    task.nome.toLocaleLowerCase().includes(search.toLocaleLowerCase())
-            }
+        matchDate = taskDate >= from && taskDate <= to;
+      }
+      return matchText && matchDate;
+    });
+  }, [search, dateRange, tasks]);
 
-            let matchDate = true
-            if (dateRange) {
-                // const taskDate = parseISO(task.dataCriacao)
-                const taskDate = task.dataCriacao
-                const from = startOfDay(dateRange.from!)
-                const to = endOfDay(dateRange.to!)
+  // paginação
+  const itemsPerPage = 10;
+  const totalPages = Math.ceil(filteredData.length / itemsPerPage);
 
-                matchDate =
-                    taskDate >= from! &&
-                    taskDate <= to!
-            }
-            return matchText && matchDate
-        })
+  const paginatedData = useMemo(() => {
+    const start = (page - 1) * itemsPerPage;
+    const end = page * itemsPerPage;
 
-    }, [search, dateRange, tasks]);
+    return filteredData.slice(start, end);
+  }, [filteredData, page, itemsPerPage]);
 
-    // paginação
-    const itemsPerPage = 10;
-    const totalPages = Math.ceil(filteredData.length / itemsPerPage)
+  // reset automático quando filtros mudam
+  const setPageSafe = (newPage: number) => {
+    setPage(newPage);
+  };
 
-    const paginatedData = useMemo(() => {
+  const resetPage = () => {
+    setPage(1);
+  };
 
-        const start = (page - 1) * itemsPerPage
-        const end = page * itemsPerPage
+  return {
+    page,
+    setPage: setPageSafe,
+    resetPage,
+    handleSearch,
+    search,
+    filteredData,
+    dateRange,
+    setDateRange,
+    handleDateSearch,
+    paginatedData,
 
-        return filteredData.slice(start, end)
-
-    }, [filteredData, page, itemsPerPage])
-
-
-    // reset automático quando filtros mudam
-    const setPageSafe = (newPage: number) => {
-        setPage(newPage)
-    }
-
-    const resetPage = () => {
-        setPage(1)
-    }
-
-
-    return {
-        page,
-        setPage: setPageSafe,
-        resetPage,
-        handleSearch,
-        search,
-        filteredData,
-        dateRange,
-        setDateRange,
-        handleDateSearch,
-        paginatedData,
-
-        totalPages,
-        totalItems: filteredData.length
-    }
+    totalPages,
+    totalItems: filteredData.length,
+  };
 }
